343c93a0e08e6f899dba84efad5d955fabe9a023
Ester Veiga <esterdcveiga@gmail.com>
2022-07-14 13:55:23 -0300

    criando BASE_URL no arquivo enviroment.ts e usando-o nas rotas no service

--- src/app/contato.service.ts
+++ src/app/contato.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Icontato } from './interfaces/Icontato';
 
 //anotação injetable permite que esse componente possa ser injetado em outro
@@ -12,16 +13,16 @@ export class ContatoService {
   constructor(private http:HttpClient) { }
 
   getContatos(){
-    return this.http.get('http://localhost:3000/contatos')
+    return this.http.get(`${environment.BASE_URL}contatos`)
   }
   save(data:any){
-    return this.http.post('http://localhost:3000/contatos', data)
+    return this.http.post(`${environment.BASE_URL}contatos`, data)
   }
   getUmContato(id:number){
-    return this.http.get(`http://localhost:3000/contatos/${id}`)
+    return this.http.get(`${environment.BASE_URL}contatos/${id}`)
   }
   alterarContato(dados:any){
-    return this.http.put(`http://localhost:3000/contatos/${dados.id}`, dados)
+    return this.http.put(`${environment.BASE_URL}contatos/${dados.id}`, dados)
   }
 }
 
