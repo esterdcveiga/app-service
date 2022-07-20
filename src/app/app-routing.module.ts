8cfced24adb8326826cf57c37d7c8fee3fabe81e
Ester Veiga <esterdcveiga@gmail.com>
2022-07-20 15:36:07 -0300

    criando componente usuário e integrando o cadastro de usuário com o backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './auth-guard.service';
+import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { ConsultaComponent } from './consulta/consulta.component';
 import { EdicaoComponent } from './edicao/edicao.component';
 import { LoginComponent } from './login/login.component';
 import { PipesComponent } from './pipes/pipes.component';
+import { UsuarioService } from './usuario.service';
 
 const routes: Routes = [
   {
@@ -29,6 +31,10 @@ const routes: Routes = [
   {
     path: 'pipes',
     component: PipesComponent
+  },
+  {
+    path: 'usuario',
+    component: CadastroUsuarioComponent
   }
 ];
 
